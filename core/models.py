def7b6d34e7bc3ba482a5d5fbc7be6776237dfd7
thanhdv <thanhcoral@gmail.com>
2022-11-15 15:17:45 +0700

    up

--- core/models.py
+++ core/models.py
@@ -39,14 +39,16 @@ class CustomerContact(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='customer_contact')
     def __str__(self):
         return f"{self.customer} [{self.label}]"
-
-
+class Supplier(models.Model):
+    name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
 class Component(models.Model):
     name = models.CharField(max_length=50)
     type = models.CharField(max_length=50, choices=COMPONENT_TYPE, blank=True, null=True)
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE, blank=True, null=True)
     def __str__(self):
         return self.name
-
 class Product(models.Model):
     name = models.CharField(max_length=50)
     type = models.CharField(max_length=50, choices=PRODUCT_TYPE, blank=True, null=True)
@@ -59,24 +61,20 @@ class Product(models.Model):
         for ivt in ivts:
             total += ivt.quantity
         return total
-
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     order_date = models.DateTimeField(default=timezone.now, blank=True, null=True)
     status = models.CharField(max_length=50, choices=ORDER_STATUS, blank=True, null=True)
     def __str__(self):
         return f"{self.customer} [{self.order_date.strftime('%d-%m-%y')}]"
-
 class OrderLine(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE, unique=True)
     quantity = models.IntegerField()
-
 class BOM(models.Model):
     code = models.CharField(max_length=5, blank=True, null=True, unique=True)
     def __str__(self):
         return self.code
-
 class ManufacturingPlan(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     bom = models.ForeignKey(BOM, on_delete=models.CASCADE, blank=True, null=True)
@@ -89,12 +87,10 @@ class ManufacturingPlan(models.Model):
         return f"{self.order.customer} [{self.order.order_date.strftime('%d-%m-%y')}]"
     def duration(self):
         return (self.end_date-self.start_date).days
-
 class Team(models.Model):
     name = models.CharField(max_length=50)
     def __str__(self):
         return self.name
-
 class Task(models.Model):
     plan = models.ForeignKey(ManufacturingPlan, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
@@ -109,13 +105,21 @@ class Task(models.Model):
         return (self.end_date-self.start_date).days
     def progress(self):
         return 1.0*(self.quantity_process/self.quantity)
-        
 class Warehouse(models.Model):
     name = models.CharField(max_length=50)
     def __str__(self):
         return self.name
-
 class Inventory(models.Model):
     warehouse = models.ForeignKey(Warehouse, on_delete=models.CASCADE, blank=True, null=True)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField()
+
+
+
+
+
+
+
+
+
+
